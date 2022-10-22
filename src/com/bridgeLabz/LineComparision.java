eb557c0b0ea47549d79bb8d4918be7ac0637bf08
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 <tusharpatil22700@gmail.com>
2022-10-21 22:27:30 -0700

    welcome

---
+++ src/com/bridgeLabz/LineComparision.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz;
+
+public class LineComparision {
+	
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to the Line And Comparison Programs");
+
+	}
+
+}
