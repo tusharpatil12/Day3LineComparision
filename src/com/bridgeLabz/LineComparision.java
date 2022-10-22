cee84a91a8ca02635246af64481554f8d306e8ab
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 <tusharpatil22700@gmail.com>
2022-10-21 22:46:07 -0700

    LineCompaision

--- src/com/bridgeLabz/LineComparision.java
+++ src/com/bridgeLabz/LineComparision.java
@@ -1,11 +1,16 @@
 package com.bridgeLabz;
 
 public class LineComparision {
-	
+	 static void length(int x1 , int y1, int x2, int y2){
+
+
+	        double Line = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+	        System.out.println(Line);
+	    }
 
 	public static void main(String[] args) {
 		 System.out.println("Welcome to the Line And Comparison Programs");
-
+		 length(1,1,5,5);
 	}
 
 }
